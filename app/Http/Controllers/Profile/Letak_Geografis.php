faaa3ca85dd780d275d9a24817022c21070fc9e8
BeniSyach <benisyach32@gmail.com>
2023-06-13 16:46:41 +0700

    perubahan data profil kecamatan

--- app/Http/Controllers/Profile/Letak_Geografis.php
+++ app/Http/Controllers/Profile/Letak_Geografis.php
@@ -11,9 +11,12 @@ use Illuminate\Support\Facades\Request as FacadesRequest;
 class Letak_Geografis extends Controller
 {
     public function index(){
-        
+        // ambil url domain
         $GetDomain = FacadesRequest::getHost();
         $domain = Kecamatan::where('domain_kecamatan',$GetDomain)->first();
+        // get kode Kecamatan
+        $get_kd_kecamatan = $domain['kode_kecamatan'];
+        
 
         return Inertia::render('Profile/LetakGeografis',[
             'judul'=>'Letak Geografis',
