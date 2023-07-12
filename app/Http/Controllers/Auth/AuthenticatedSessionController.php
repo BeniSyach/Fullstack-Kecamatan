354e1850d22a02868598c870e028e3e602b885bd
BeniSyach <benisyach32@gmail.com>
2023-07-12 14:32:08 +0700

    change auth register

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -34,6 +34,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
+        
 
 
         return redirect()->intended(RouteServiceProvider::HOME)->with('message', 'Anda Berhasil Login');
