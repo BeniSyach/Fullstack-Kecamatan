639fb9e40d9c86d10327c13de765fe481a23cf03
BeniSyach <benisyach32@gmail.com>
2023-07-18 12:09:30 +0700

    change data seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,25 +13,30 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            data_domain_kecamatan::class,
-            data_sejarah_kecamatan::class,
-            data_letak_geografis::class,
-            data_kependudukan::class,
-            data_visi_dan_misi::class,
-            data_slider::class,
             data_adat_dan_budaya::class,
-            data_tupoksi::class,
-            data_kepegawaian::class,
-            data_prestasi::class,
+            data_agenda::class,
+            data_berita::class,
+            data_detail_pelayanan::class,
+            data_detail_potensi::class,
+            data_detail_unduhan::class,
+            data_domain_kecamatan::class,
+            data_foto::class,
             data_kata_sambutan::class,
             data_kategori_berita::class,
+            data_kepegawaian::class,
+            data_kependudukan::class,
+            data_letak_geografis::class,
+            data_list_pelayanan::class,
+            data_list_unduhan::class,
+            data_potensi::class,
+            data_prestasi::class,
+            data_sejarah_kecamatan::class,
+            data_slider::class,
             data_struktur_organisasi::class,
-            data_berita::class,
-            data_foto::class,
+            data_tupoksi::class,
             data_video::class,
+            data_visi_dan_misi::class,
             data_wisata::class,
-            data_agenda::class,
-            data_potensi::class
         ]);
     }
 }
